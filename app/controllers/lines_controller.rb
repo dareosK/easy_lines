c84df7d6962bd4598f29e2216c780d1379c255c3
dareos khalili <khalilidareos@gmail.com>
2025-01-27 21:50:28 +0100

    adds controlers, routes and starts dashbpoard feature

---
+++ app/controllers/lines_controller.rb
@@ -0,0 +1,7 @@
+class LinesController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      redirect_to dashboard_path # Replace with the actual dashboard path
+    else
+      redirect_to new_user_registration_path
+    end
+  end
+
+  def dashboard
   end
 end
